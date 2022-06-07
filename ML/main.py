d9de814aae8b40789af42a6cf0ce732066987194
s160419091 <s160419091@student.ubaya.ac.id>
2022-06-07 13:10:23 +0700

    update ml

---
+++ ML/main.py
@@ -0,0 +1,55 @@
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+import numpy as np
+import tensorflow as tf
+import re
+
+app = FastAPI()
+class Emotion(BaseModel):
+    sentence: str
+
+@app.get('/')
+def index():
+    return {'message' : 'Hello World'}
+    
+
+@app.get('/predict')
+def predict():
+    # print(tf.Session())
+    with open('tokenizer.pickle', 'rb') as handle:
+        Tokenizer = pickle.load(handle)
+
+    model = tf.keras.models.load_model("model_final.model", device='cuda')
+    # with open('model_pkl' , 'rb') as f:
+    #     model = pickle.load(f)
+    padding_type='post'
+    max_length = 32
+
+    sentence = 'i feel shocked'
+    sentence = preprocess_text(sentence)
+
+    sentence_processed = Tokenizer.texts_to_sequences([sentence])
+    sentence_processed = np.array(sentence_processed)
+    sentence_padded = tf.keras.preprocessing.sequence.pad_sequences(sentence_processed, padding=padding_type, maxlen=max_length)
+    predicted = model.predict(sentence_padded)
+    no_class = np.argmax(predicted)
+    return {
+    'prediction': no_class,
+}
+
+def preprocess_text(sen):
+    sentence = remove_tags(sen)
+
+    sentence = re.sub('[^a-zA-Z]', ' ', sentence)
+
+    sentence = re.sub(r"\s+[a-zA-Z]\s+", ' ', sentence)
+
+    sentence = re.sub(r'\s+', ' ', sentence)
+
+    return sentence
+TAG_RE = re.compile(r'<[^>]+>')
+
+def remove_tags(text):
+    return TAG_RE.sub('', text)
